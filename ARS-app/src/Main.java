6ed7147c9e04027cdb25858a19032f0db7cf2632
heisdanielade <heisdanielade@gmail.com>
2025-06-05 13:17:05 +0200

    udpate: minor changes

--- ARS-app/src/Main.java
+++ ARS-app/src/Main.java
@@ -1,10 +1,22 @@
 
+import view.*;
+import javafx.application.Application;
+import javafx.stage.Stage;
 import util.DatabaseManager;
 
 import java.sql.*;
 
-public class Main {
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+//        new FlightView().start(primaryStage);
+//        new FlightSeatReportView().start(primaryStage);
+//        new ClientSummaryView().start(primaryStage);
+//        new EmployeeSummaryView().start(primaryStage);
+        new MainMenuView().start(primaryStage);
+    }
     public static void main(String[] args) throws SQLException {
+        launch(args);
         DatabaseManager.getConnection();
     }
 }
--- ARS-app/src/model/Flight.java
+++ ARS-app/src/model/Flight.java
@@ -6,6 +6,42 @@ public class Flight {
     private int flightId;
     private LocalDateTime baseDepartureTime;
     private LocalDateTime baseArrivalTime;
+    private int departureAirportId;
+    private int arrivalAirportId;
+    private int aircraftId;
+
+    public int getDepartureAirportId() {
+        return departureAirportId;
+    }
+
+    public void setDepartureAirportId(int departureAirportId) {
+        this.departureAirportId = departureAirportId;
+    }
+
+    public int getArrivalAirportId() {
+        return arrivalAirportId;
+    }
+
+    public void setArrivalAirportId(int arrivalAirportId) {
+        this.arrivalAirportId = arrivalAirportId;
+    }
+
+    public int getAircraftId() {
+        return aircraftId;
+    }
+
+    public void setAircraftId(int aircraftId) {
+        this.aircraftId = aircraftId;
+    }
+
+    public Flight(int flightId, LocalDateTime baseDepartureTime, LocalDateTime baseArrivalTime, int departureAirportId, int arrivalAirportId, int aircraftId) {
+        this.flightId = flightId;
+        this.baseDepartureTime = baseDepartureTime;
+        this.baseArrivalTime = baseArrivalTime;
+        this.departureAirportId = departureAirportId;
+        this.arrivalAirportId = arrivalAirportId;
+        this.aircraftId = aircraftId;
+    }
 
     public int getFlightId() {
         return flightId;
