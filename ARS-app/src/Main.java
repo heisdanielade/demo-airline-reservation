acb4507f0abbfa5de67c084236fbea8d1cc2ffcb
heisdanielade <heisdanielade@gmail.com>
2025-06-05 13:33:00 +0200

    chore: minor changes

--- ARS-app/src/Main.java
+++ ARS-app/src/Main.java
@@ -9,10 +9,6 @@ import java.sql.*;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
-//        new FlightView().start(primaryStage);
-//        new FlightSeatReportView().start(primaryStage);
-//        new ClientSummaryView().start(primaryStage);
-//        new EmployeeSummaryView().start(primaryStage);
         new MainMenuView().start(primaryStage);
     }
     public static void main(String[] args) throws SQLException {
